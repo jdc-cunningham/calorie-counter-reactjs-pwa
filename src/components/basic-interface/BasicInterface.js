1c7fbba941f38df08a3458f3242f724cd3cdd5ea
jdc-cunningham <jcun4128@gmail.com>
2019-07-25 00:38:55 -0500

    the state is a cluster f, need to clean up the stringify/parse and keep track of what is being stored into sessionStorage

--- src/components/basic-interface/BasicInterface.js
+++ src/components/basic-interface/BasicInterface.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import './BasicInterface.scss';
 
 class BasicInterface extends Component {
+  state = {
+    today: this.getTodaysDate(),
+    calories : 0, // this.getTodaysCalories(),
+    entries: [] // this.getTodaysCalorieEntries(),
+  }
+
+  addCalories = this.addCalories.bind( this );
+  getTodaysCalories = this.getTodaysCalories.bind( this );
+  getTodaysCalorieEntries = this.getTodaysCalorieEntries.bind( this );
+
   getTodaysDate() {
     // https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
 
@@ -10,17 +20,18 @@ class BasicInterface extends Component {
         mm = String(today.getMonth() + 1).padStart(2, '0'), //January is 0!
         yyyy = today.getFullYear();
 
-    today = mm + '/' + dd + '/' + yyyy;
+    today = mm + '-' + dd + '-' + yyyy;
     return today;
   }
 
   getTodaysCalories() {
-    const todaysCalories = sessionStorage.getItem( this.getTodaysDate() );
-    return todaysCalories ? todaysCalories : 0;
+    // console.log( this.state );
+    let todaysCalories = sessionStorage.getItem( this.state.today );
+    return todaysCalories ? JSON.parse( todaysCalories ).total : 0;
   }
 
   updateCalories( dateStr, calories, add ) {
-    let daysCalories = sessionStorage.getItem( dateStr ),
+    let daysCalories = sessionStorage.getItem( dateStr ) ? JSON.parse( sessionStorage.getItem(dateStr) ).total : 0,
         newCalories = 0;
 
     if ( add ) {
@@ -34,22 +45,55 @@ class BasicInterface extends Component {
     sessionStorage.setItem( dateStr, newCalories );
   }
 
-  state = {
-    calories : this.getTodaysCalories()
+  getTodaysCalorieEntries() {
+    const todaysCalorieEntries = sessionStorage.getItem( this.state.today );
+    return todaysCalorieEntries ? JSON.parse( todaysCalorieEntries ).entries : [];
   }
 
-  addTenCalories() {
+  updateSessionStorage() {
+    sessionStorage.setItem( this.state.today, JSON.stringify(this.state) );
+  }
+
+  addCalories( calories ) {
     // this.setState({
     //   calories: this.state.calories + 10
     // });
-    console.log( this.state.calories + 10 );
+    // console.log( this.state.calories + 10 );
+
+    // console.log( this.state );
+
+    let entries = this.state.entries.length ? this.state.entries : [];
+    
+    entries.push({
+      name: 'test',
+      calories: calories,
+      gain: true
+    });
+
+    console.log( entries );
+
+    this.setState( prevState => ({
+      calories: this.state.calories + calories,
+      entries: entries
+    }));
+
+    // this.updateSessionStorage();
+  }
+
+  componentDidMount() {
+    this.setState( prevState => ({
+      calories : this.getTodaysCalories(),
+      entries: this.getTodaysCalorieEntries(),
+    }));
   }
 
   render() {
+    
+
     return(
       <div className="basic-interface">
         { this.state.calories }
-        <button type="button" onClick={ this.addTenCalories }>Add 10 Calories</button>
+        <button type="button" onClick={ () => this.addCalories( 30 ) }>Add Calories</button>
       </div>
     )
   }
